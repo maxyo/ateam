db133bd5ca174d5c28c47f17e376e22b149cabfc
Maksim Ivanyushkin <maksim@ivanyushkin.email>
2022-12-25 03:35:27 +0500

    fix types

--- py/config.py
+++ py/config.py
@@ -4,14 +4,14 @@ from py.client import Client
 
 parser = argparse.ArgumentParser(prog='ateam')
 
-parser.add_argument('--wcapacity', required=False, default=200, help='Weight capacity')
-parser.add_argument('--vcapacity', required=False, default=100, help='Volume capacity')
-parser.add_argument('--bcount', required=False, default=46, help='Bags count')
-parser.add_argument('--hbcount', required=False, default=2, help='Bags with many gifts count')
+parser.add_argument('--wcapacity', required=False, type=int, default=200, help='Weight capacity')
+parser.add_argument('--vcapacity', required=False, type=int, default=100, help='Volume capacity')
+parser.add_argument('--bcount', required=False, type=int, default=46, help='Bags count')
+parser.add_argument('--hbcount', required=False, type=int, default=2, help='Bags with many gifts count')
 parser.add_argument('--output', required=False, default='stdout', help='Output path or stdout')
-parser.add_argument('--debug', required=False, default=False, help='Debug mode')
+parser.add_argument('--debug', required=False, type=bool, default=False, help='Debug mode')
 
-parser.add_argument('--blimit', required=False, default=2, help='Bags combination searching time limit, seconds')
+parser.add_argument('--blimit', required=False, type=int, default=2, help='Bags combination searching time limit, seconds')
 
 args = parser.parse_args()
 
