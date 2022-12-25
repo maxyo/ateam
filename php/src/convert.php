78ec8dd2110a3630d853bb095a0ccfcf9274f69a
Andrey Pakhomov <andrey.pahomov@gmail.com>
2022-12-25 21:53:07 +0300

    Добавлен конвертер

---
+++ php/src/convert.php
@@ -0,0 +1,11 @@
+<?php
+
+$json = file_get_contents('moves_last.json');
+$clusters = json_decode($json, true);
+$moves = [];
+foreach ($clusters as $pointsArray) {
+    $move = array_map(fn($it) => ['x'=>$it[0], 'y'=>$it[1]], $pointsArray);
+    $moves[] = $move;
+}
+
+file_put_contents("moves-xy_last.json", json_encode($moves));
