0b18d15b8e04a87c084b76555b546090509e5c3a
Andrey Pakhomov <andrey.pahomov@gmail.com>
2022-12-26 16:11:38 +0300

    Update circle_around.py

--- py/circle_around.py
+++ py/circle_around.py
@@ -34,22 +34,32 @@ def get_intersect(cx, cy, cr, x0, y0, x1, y1):
 def get_points_outside(points, cx, cy, cr, x0, y0, x1, y1, count=20):
     h = dist([x0, y0], [x1, y1])
     print("Dist: ", h, h / 2 / cr)
-    rad = 180 / math.pi;
-    a = math.asin(h / 2 / cr) * rad * 2;
-    da = a / count;
+    rad = 180 / math.pi
+    a = math.asin(h / 2 / cr) * 2 * rad
+    [xe, ye] = rotate2D(a, cx, cy, x0, y0)
+    [xs, ys] = rotate2D(-a, cx, cy, x0, y0)
+    if round(xs, 2) == round(x1, 2) and round(ys, 2) == round(y1, 2):
+        a = - a
+    print("x1,y1: ", x1, y1, "xe,ye: ", xe, ye, "xs,ys: ", xs, ys)
+    print("Alpha: ", a, "Radius: ", dist([cx, cy], [x0, y0]), dist([cx, cy], [x1, y1]))
+    da = a / (count - 1)
     points.append([x0, y0])
+    xi = x0
+    yi = y0
     for i in range(count - 1):
-        # [xi, yi] = rotate2D(da * (i + 1), cx, cy, x0, y0)
-        [xi, yi] = rotate2D(da * (i + 1), cx, cy, x0, y0)
+        angle = da * (i + 1)
+        [xi, yi] = rotate2D(da, cx, cy, xi, yi)
         points.append([xi, yi])
-        print(xi, yi)
+        print(angle, xi, yi)
 
     points.append([x1, y1])
+    print(x1, y1)
     return points
 
 
 def dist(p1, p2):
     return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
+
 
 # Строит маршрут огибания бури
 # cx - X coordinate of circle`s center
@@ -58,24 +68,32 @@ def dist(p1, p2):
 # x0, y0 - coordinates of first point
 # x1, y1 - coordinates of second point
 # return [[x0,y0],[x1,y1],...,[xn,yn]] - array of points, includes start and end points of route
-def get_route(cx,cy,cr, x0, y0, x1, y1):
-    i = get_intersect(cx,cy,cr, x0, y0, x1, y1);
-    points = [[0, 0]]
+def get_route(cx, cy, cr, x0, y0, x1, y1):
+    i = get_intersect(cx, cy, cr, x0, y0, x1, y1);
+    print("Intersect: ", dist([cx, cy], i[0]), dist([cx, cy], i[1]))
+    points = [[x0, y0]]
     if i:
-        dist0 = dist([0, 0], i[0])
-        dist1 = dist([0, 0], i[1])
-        if (dist0<dist1):
-            points = get_points_outside(points, 5, 5, 3, i[0][0], i[0][1], i[1][0], i[1][1]);
+        dist0 = dist([x0, y0], i[0])
+        dist1 = dist([x1, y1], i[1])
+        if dist0 < dist1:
+            points = get_points_outside(points, cx, cy, cr, i[0][0], i[0][1], i[1][0], i[1][1]);
         else:
-            points = get_points_outside(points, 5, 5, 3, i[1][0], i[1][1], i[0][0], i[0][1]);
-    points.append([10, 10])
+            points = get_points_outside(points, cx, cy, cr, i[1][0], i[1][1], i[0][0], i[0][1]);
+    points.append([x1, y1])
     return points
 
-points = get_route(5, 5, 3, 0, 0, 10, 10);
+
+c = [8, 6, 3];
+points = get_route(c[0], c[1], c[2], 0, 0, 10, 10)
 x, y = zip(*points)
 print("Points: ", points)
+
+plt.gca().set_xscale('linear')
+fig, ax = plt.subplots()
+circle = plt.Circle((c[0], c[1]), c[2], color='r')
 plt.scatter(x, y)
-plt.savefig('foo.png')
+ax.add_patch(circle)
+fig.savefig('foo.png')
 
 # print(i.geoms[0].coords[0])
 # print(i.geoms[1].coords[0])
